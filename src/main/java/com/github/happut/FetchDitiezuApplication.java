d696db462363d1906fc796fdf5c59beb4a076fdd
bjwangfei3 <bjwangfei3@corp.netease.com>
2017-05-16 15:16:18 +0800

    init

---
+++ src/main/java/com/github/happut/FetchDitiezuApplication.java
@@ -0,0 +1,12 @@
+package com.github.happut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchDitiezuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchDitiezuApplication.class, args);
+	}
+}
